8b7e9807be568b014ccdcb6f33cc20e700053391
finwe108 <nice.atma@gmail.com>
2017-10-01 19:43:33 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,8 @@
 <?php
 // Database constants
 
-defined('DB_SERVER') ? null : define('DB_SERVER', 'localhost');
-defined('DB_USER') ? null : define('DB_USER', 'root');
-defined('DB_PASS') ? null : define('DB_PASS', '');
-defined('DB_NAME') ? null : define('DB_NAME', 'school');
-defined('APP_NAME') ? null : define('APP_NAME', 'Madana Mohana Academy');
+defined('DB_SERVER') ? null : define('DB_SERVER', 'localhost'); // host
+defined('DB_USER') ? null : define('DB_USER', 'root'); // username 
+defined('DB_PASS') ? null : define('DB_PASS', ''); // password
+defined('DB_NAME') ? null : define('DB_NAME', 'school'); // the name of your database
 ?>
